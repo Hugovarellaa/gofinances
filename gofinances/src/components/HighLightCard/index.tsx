17baf109c6716211a33b3f7385fc9ad1f41c3694
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-30 19:37:16 -0300

    Layout HighLightCard terminado

--- gofinances/src/components/HighLightCard/index.tsx
+++ gofinances/src/components/HighLightCard/index.tsx
@@ -10,21 +10,30 @@ import {
 } from "./styles";
 
 interface Props {
-
+  type: "up" | "down" | "total";
+  title: string;
+  amount: string;
+  lastTransaction: string;
 }
 
-export function HighLightCard({}:Props) {
+const icon = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+  total: "dollar-sign",
+};
+
+export function HighLightCard({ title, amount, lastTransaction, type }: Props) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entradas</Title>
-        <Icon name="arrow-up-circle" />
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type} />
       </Header>
 
-        <Footer>
-          <Amount>R$ 17.400,00</Amount>
-          <LastTransaction>Última entrada dia 13 de abril</LastTransaction>
-        </Footer>
+      <Footer>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>{lastTransaction}</LastTransaction>
+      </Footer>
     </Container>
   );
 }
