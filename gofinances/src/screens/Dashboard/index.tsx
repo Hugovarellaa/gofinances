d936b06ff3b9e29441b03fd212eea2e66fd08721
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-30 18:18:27 -0300

    Layout Dashboard Header

--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Container,Title } from "./styles";
+import React from "react";
+import {
+  Container,
+  Header,
+  UserInf,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  HeaderWrapper,
+  Icon,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <>
       <Container>
-        <Title>Dashboard</Title>
+        <Header>
+          <HeaderWrapper>
+            <UserInf>
+              <Photo
+                source={{
+                  uri: "https://github.com/Hugovarellaa.png",
+                }}
+              />
+              <User>
+                <UserGreeting>Olá</UserGreeting>
+                <UserName>Hugo</UserName>
+              </User>
+            </UserInf>
+            <Icon name="power" />
+          </HeaderWrapper>
+        </Header>
       </Container>
     </>
-  )
+  );
 }
-
--- gofinances/src/screens/Dashboard/styles.ts
+++ gofinances/src/screens/Dashboard/styles.ts
@@ -1,14 +1,61 @@
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
 
-  background: ${({theme}) => theme.colors.background}
+  background: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  color: ${props => props.theme.colors.primary};
-  font-family: ${({theme}) => theme.fonts.regular};
+export const Header = styled.View`
+  width: 100%;
+  background: ${({ theme }) => theme.colors.primary};
+  height: ${RFPercentage(36)}px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const HeaderWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const UserInf = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.secondary};
+` as unknown as typeof Feather;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  margin-bottom: -10px;
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(18)}px;
 `;
