b3bfe70de7e1a7027b707f36d9d38b476a295622
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-01 09:50:42 -0300

    Delete gofinances directory

--- gofinances/src/global/styles/styles.d.ts
+++
@@ -1,9 +0,0 @@
-import "styled-components/native";
-import theme from "./theme";
-
-declare module "styled-components/native" {
-  type ThemeType = typeof theme;
-
-  export interface DefaultTheme extends ThemeType {}
-}
-   
