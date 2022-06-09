b405be20028c7cfa72ef9189917fd5e5d00b4eac
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 <hugovarellaa@gmail.com>
2022-06-09 09:20:40 -0300

    Estruturando o Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import {
+  Avatar,
+  Container,
+  Header,
+  User,
+  UserContainer,
+  UserGreeting,
+  UserInfo,
+  UserName
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserContainer>
+          <UserInfo>
+            <Avatar source={{ uri: "https://github.com/Hugovarellaa.png" }} />
+
+            <User>
+              <UserGreeting>Ola,</UserGreeting>
+              <UserName>Hugo Varella</UserName>
+            </User>
+          </UserInfo>
+        </UserContainer>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,48 @@
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  color: ${({ theme }) => theme.colors.text_dark};
-  font-size: 24px;
-  font-family: ${({ theme }) => theme.fonts.medium};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const UserContainer = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(16)}px;
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(16)}px;
 `;
