f4e3c0390cd2091f94a83fae26b338a8ce9194b2
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-16 18:30:20 -0300

    Delete gofinances directory

--- gofinances/src/@types/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import 'styled-components'
-import theme from '../styles/theme/default'
-
-declare module 'styled-components' {
-  type ThemeType = typeof theme 
-  export interface DefaultTheme extends ThemeType {}
-}
