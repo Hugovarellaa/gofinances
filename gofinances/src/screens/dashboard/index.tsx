2f52b3bef48e3ee7a5abef464b9279708ec20f33
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-17 08:58:37 -0300

    Layout header

--- gofinances/src/screens/dashboard/index.tsx
+++ gofinances/src/screens/dashboard/index.tsx
@@ -1,10 +1,25 @@
-import { DashboardContainer, Header } from "./styles";
+import {
+  AvatarUser, DashboardContainer,
+  Header, Icon, User, UserGreeting, UserInfo, UserName, UserWrapper
+} from "./styles";
 
-export function Dashboard (){
+export function Dashboard() {
   return (
     <DashboardContainer>
       <Header>
-        
+        <UserWrapper>
+          <User>
+            <AvatarUser source={{
+              uri: 'https://github.com/Hugovarellaa.png'
+            }}
+            />
+            <UserInfo>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Hugo Varella</UserName>
+            </UserInfo>
+          </User>
+          <Icon name='power' />
+        </UserWrapper>
       </Header>
     </DashboardContainer>
   )
--- gofinances/src/screens/dashboard/styles.ts
+++ gofinances/src/screens/dashboard/styles.ts
@@ -1,13 +1,59 @@
-import { RFPercentage } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const DashboardContainer = styled.View`
   flex:1;
   background: ${({theme}) => theme.colors.background};
+  
 `
 
 export const Header = styled.View`
   width:100%;
   background: ${({theme}) => theme.colors.primary};
   height: ${RFPercentage(36)}px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 0 24px;
+`
+
+export const User = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const AvatarUser = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 5px;
+  overflow: hidden;
+  margin-right: 17px;
+`
+
+export const UserInfo = styled.View``
+
+export const UserGreeting = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-size: ${RFValue(17)}px;
+  font-family: ${({theme}) => theme.fonts.regular};
+`
+
+export const UserName = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-size: ${RFValue(17)}px;
+  font-family: ${({theme}) => theme.fonts.bold};
+  margin-top: -8px;
+`
+
+export const Icon = styled(Feather)`
+  color: ${({theme}) => theme.colors.attention};
+  font-size: ${RFValue(24)}px;
+` 
