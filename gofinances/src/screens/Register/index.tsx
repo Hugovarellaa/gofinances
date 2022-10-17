b459ac187080b4a13add63f59e05c747cea02c29
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-17 17:37:18 -0300

    Rota Register

---
+++ gofinances/src/screens/Register/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { RegisterContainer } from "./style";
+
+export function Register (){
+  return (
+    <RegisterContainer>
+        <Text>Testando</Text>
+    </RegisterContainer>
+  )
+}
---
+++ gofinances/src/screens/Register/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const RegisterContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
