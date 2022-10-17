a10a8e38b8396e4b88cefd65122d3819b52a0804
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-17 07:46:38 -0300

    Styled components

---
+++ gofinances/src/screens/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { DashboardContainer, Title } from "./styles";
+
+export function Dashboard (){
+  return (
+    <DashboardContainer>
+      <Title>Testando</Title>
+    </DashboardContainer>
+  )
+}
---
+++ gofinances/src/screens/dashboard/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const DashboardContainer = styled.View`
+  flex:1;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.Text``
