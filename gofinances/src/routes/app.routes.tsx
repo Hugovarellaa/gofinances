5e24949967bc82e5446193833aeeaf0b17bb7a54
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-18 16:38:27 -0300

    Formulário e navegação completed

--- gofinances/src/routes/app.routes.tsx
+++ gofinances/src/routes/app.routes.tsx
@@ -1,15 +1,54 @@
+import { MaterialIcons } from "@expo/vector-icons"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Platform } from "react-native"
+import { useTheme } from "styled-components"
 import { Dashboard } from "../screens/dashboard"
 import { Register } from "../screens/Register"
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export function AppRoutes() {
+  const { colors } = useTheme()
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.secondary,
+        tabBarInactiveTintColor: colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 68,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0
+        }
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <MaterialIcons name='format-list-bulleted' size={size} color={color} />
+          )
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <MaterialIcons name='attach-money' size={size} color={color} />
+          )
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <MaterialIcons name='pie-chart' size={size} color={color} />
+          )
+        }}
+      />
     </Navigator>
   )
 }
